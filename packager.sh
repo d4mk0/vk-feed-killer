7855133b4435bd8508375ae3a2fa216482140472
d4mk0 <d4mk0f@gmail.com>
2015-06-27 11:29:03 +0300

    packeger script for creating builds was added

---
+++ packager.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir tmp
+cp manifest.json tmp/
+cp icons/* tmp/
+cp jquery-2.1.4.min.js tmp/
+cp runner.js tmp/
+cp styles.css tmp/
+
+REVISION=$(git describe --always --tag)
+zip -r builds/vk-feed-killer_$REVISION.zip tmp
+rm -rf tmp/
